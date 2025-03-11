d012c5b2f53501f3a8762e51b34f719eef49056b
Adan Esaid <adanesaid.2001@gmail.com>
2025-03-11 18:37:30 +0200

    CartoonItem.tsx

--- src/components/CartoonItem.tsx
+++ src/components/CartoonItem.tsx
@@ -1,5 +1,4 @@
-
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 interface CartoonProps {
   id: number;
@@ -10,8 +9,17 @@ interface CartoonProps {
 }
 
 const CartoonItem: React.FC<CartoonProps> = ({ name, image, species, status }) => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (divRef.current) {
+      console.log("Div Element Loaded:", divRef.current);
+      divRef.current.style.border = "2px solid blue";
+    }
+  }, []);
+
   return (
-    <div className="cartoon-item">
+    <div className="cartoon-item" ref={divRef}>
       <img src={image} alt={name} />
       <h3>{name}</h3>
       <p><strong>Species:</strong> {species}</p>
